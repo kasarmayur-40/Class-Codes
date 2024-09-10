561a2d64405353f360466bd0313a4b4e957a482f
kasarmayur-40 <kasarm047@gmail.com>
2024-09-10 09:52:57 +0530

    first commit

--- Assignment_6/program6.dart
+++ Assignment_6/program6.dart
@@ -1,3 +1,12 @@
+import "dart:io";
 void main(){
+  print("Enter number");
+  int num=int.parse(stdin.readLineSync()!);
+  int count=0;
 
+  while(num>0){
+    count++;
+    num~/=10;
+  }
+  print(count);
 }
